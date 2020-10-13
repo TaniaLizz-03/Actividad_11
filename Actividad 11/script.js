337f1bdd0e0eb22893796a8b864f8e949293cc27
TaniaLizz-03 <71456511+TaniaLizz-03@users.noreply.github.com>
2020-10-13 16:20:09 -0600

    Add files via upload

---
+++ Actividad 11/script.js
@@ -0,0 +1,43 @@
+const clasesParadigmas = () =>{
+    setTimeout(()=>{
+    console.log("tomar clases de paradigmas");
+    }, 5000);
+}
+
+const desayunar = () =>{
+    setTimeout(()=>{
+    console.log("Desayunar");
+    }, 8000);
+}
+
+const jugar = () =>{
+    setTimeout(()=>{
+    console.log("Jugar Videojuegos");
+    }, 10000);
+}
+
+const levantarse = () =>{
+    setTimeout(()=>{
+    console.log("Levantarse");
+    }, 5000);
+}
+
+const tenderCama = () =>{
+    setTimeout(()=>{
+    console.log("tender la Cama");
+    }, 3000);
+}
+
+const banarse = () =>{
+    setTimeout(()=>{
+    console.log("Bañarse");
+    }, 3000);
+}
+
+levantarse();
+tenderCama();
+banarse();
+clasesParadigmas();
+desayunar();
+jugar();
+
